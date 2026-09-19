f336e3d71e2bc658fd457b879e3d30613aee5e8d
damodar13 <vdamodar822@gmail.com>
2026-09-19 15:52:19 +0530

    fix backend connection

--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import requests
 import pandas as pd
+import os
+from dotenv import load_dotenv
 
-API_URL = "http://127.0.0.1:8000"
+load_dotenv()  # Load environment variables from .env file
+
+API_URL = os.getenv("API__URL","http://127.0.0.1:8000")
+
+
 
 st.set_page_config(
     page_title="Employee & Payroll Portal",
